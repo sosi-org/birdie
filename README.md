60ee0ab633ce31b4d1d02bfb9597f45f1f3316fe
sohale <sohale@gmail.com>
2016-03-27 19:11:13 +0100

    Birdie is the word

---
+++ README.md
@@ -0,0 +1,1 @@
+Let's bird the word
---
+++ birdie.js
@@ -0,0 +1,62 @@
+
+
+Tasks = new Mongo.Collection("tasks");
+
+ 
+
+if (Meteor.isClient) {
+ Template.body.helpers({
+    tasks: function () {
+      // return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+      //return Tasks.find({});
+    },
+    count: function (){
+      return Tasks.find().count();
+    }
+  });
+}
+
+
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+
+if (Meteor.isClient) {
+
+  Template.body.events({
+
+    "submit .new-task": function (event) {
+      event.preventDefault();
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+      event.target.text.value = "";
+    }
+  });
+
+}
