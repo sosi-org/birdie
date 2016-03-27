7c30da071cdfbaacc5d8e592f706cbdd2a6f1cf2
sohale <sohale@gmail.com>
2016-03-27 20:09:55 +0100

    Make Facebook login work! on localhost

--- birdie.js
+++ birdie.js
@@ -59,4 +59,29 @@ if (Meteor.isClient) {
     }
   });
 
+
+
+
+Template.login.events({
+    'click #facebook-login': function(event) {
+        Meteor.loginWithFacebook({}, function(err){
+            if (err) {
+                throw new Meteor.Error("Facebook login failed");
+            }
+        });
+    },
+ 
+    'click #logout': function(event) {
+        Meteor.logout(function(err){
+            if (err) {
+                throw new Meteor.Error("Logout failed");
+            }
+        })
+    }
+});
+
+
 }
+
+
+
---
+++ server/social-config.js
@@ -0,0 +1,10 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '978333035577967',
+    secret: 'd9720f729f5d0156bf61d39ca9ef0429'
+});
+
